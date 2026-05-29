4877c11250d607f6e31eea80ad81083ad8a4d5ac
Alkesh Bakotiya <alkeshbakotiya000@gmail.com>
2026-05-29 10:08:49 +0530

    added Chatbot loop and functioon

--- fetch_Data.py
+++ fetch_Data.py
@@ -1,23 +1,44 @@
 import requests
-try:
-    city=input("Enter your City Name : ")
-    url=f"https://wttr.in/{city}?format=j1"
-    response=requests.get(url)
-    data=response.json()
-    print(data)
-    current = data["current_condition"][0]
 
-    temp = current["temp_C"]
 
-    weather = current["weatherDesc"][0]["value"]
+def get_weather(city):
 
-    humidity = current["humidity"]
-    print(f"""
-    City: {city}
-    Temperature: {temp}°C
-    
-    Weather: {weather}
-    Humidity: {humidity}%
-    """)
-except:
-    print('something went wrong ')
+    try:
+
+        url = f"https://wttr.in/{city}?format=j1"
+
+        response = requests.get(url)
+
+        data = response.json()
+
+        current = data["current_condition"][0]
+
+        temp = current["temp_C"]
+
+        weather = current["weatherDesc"][0]["value"]
+
+        humidity = current["humidity"]
+
+        print(f"""City: {city}
+             Temperature: {temp}°C
+             Weather: {weather}
+
+             Humidity: {humidity}%
+""")
+
+    except Exception as e:
+
+        print("Error:", e)
+
+
+while True:
+
+    city = input("Enter city name (or exit): ")
+
+    if city.lower() == "exit":
+
+        print("Goodbye 😄")
+
+        break
+
+    get_weather(city)
